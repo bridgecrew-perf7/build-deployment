085cc66fdc23079ee05bf2972dbe69b7f7294180
raphael-mottelet <raphael.mottelet@protonmail.com>
2021-09-10 09:51:36 +0200

    first commit, pushing build

---
+++ build/precache-manifest.7880aaa9e51bd7306a379af3889b2ac0.js
@@ -0,0 +1,486 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cf5da62a0b8cd55e2cc0cfb46a7fad4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e53d9ff4182c2621ed1",
+    "url": "/static/css/main.3b88bf1b.chunk.css"
+  },
+  {
+    "revision": "f22b670c9cf443ee248d",
+    "url": "/static/js/2.ac798106.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.ac798106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e53d9ff4182c2621ed1",
+    "url": "/static/js/main.e7e9ab7c.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  },
+  {
+    "revision": "aeb3dca0fcad3dd2831554ad9691602e",
+    "url": "/static/media/CDPr.aeb3dca0.png"
+  },
+  {
+    "revision": "16a134bb58c527f55ae65c04736b72d7",
+    "url": "/static/media/FruitsLegumes.16a134bb.JPG"
+  },
+  {
+    "revision": "4dc39e9fdd4a343e8f67965c7a0a93c3",
+    "url": "/static/media/Gaillhards.4dc39e9f.png"
+  },
+  {
+    "revision": "2a409ba117597f9e9a803703dd67c1e2",
+    "url": "/static/media/IMG_9717.2a409ba1.JPG"
+  },
+  {
+    "revision": "1b859c6ac34438a88afadc3ae1a6182b",
+    "url": "/static/media/IMG_9768.1b859c6a.JPG"
+  },
+  {
+    "revision": "16a134bb58c527f55ae65c04736b72d7",
+    "url": "/static/media/IMG_9775.16a134bb.JPG"
+  },
+  {
+    "revision": "321ed021553140f8829c9dda1c255342",
+    "url": "/static/media/IMG_9876-removebg-preview.321ed021.png"
+  },
+  {
+    "revision": "20b9e05d2b15a98f2d617ddb43dfbc3b",
+    "url": "/static/media/agrumes.20b9e05d.JPG"
+  },
+  {
+    "revision": "466dceb04f282f161a2ad6c1f20d2dc9",
+    "url": "/static/media/atillon.466dceb0.png"
+  },
+  {
+    "revision": "ce89649ada64b3545a75f9cf116a9c3d",
+    "url": "/static/media/baguette quinoa lvc.ce89649a.JPG"
+  },
+  {
+    "revision": "062dc9aade6e71a56f263e531a8a37b7",
+    "url": "/static/media/baguette trad  hervé.062dc9aa.JPG"
+  },
+  {
+    "revision": "47b0e7eb8efc2fff0a355e0aa8ccf563",
+    "url": "/static/media/baguettemultigraine-herve.47b0e7eb.JPG"
+  },
+  {
+    "revision": "8e4782a5b437ab797d31b829dca2b2bb",
+    "url": "/static/media/baguettepavot herve.8e4782a5.JPG"
+  },
+  {
+    "revision": "49b708985dbedd6e65d33fb6d6b8d954",
+    "url": "/static/media/bananes.49b70898.JPG"
+  },
+  {
+    "revision": "000c6e4b5283973ec595e2acfcd5ce9c",
+    "url": "/static/media/bio.000c6e4b.svg"
+  },
+  {
+    "revision": "94f4ee245d30b7cc41af70296fa0a436",
+    "url": "/static/media/boule epautre lvc.94f4ee24.JPG"
+  },
+  {
+    "revision": "d12fe4642e364abb64e993656d70e643",
+    "url": "/static/media/boule multi graine lvc.d12fe464.JPG"
+  },
+  {
+    "revision": "97f88cc2fe2b88ea42e9f0295f1f55d3",
+    "url": "/static/media/boulecampagneH-herve.97f88cc2.JPG"
+  },
+  {
+    "revision": "f28a1c298ad8df9ba032da795faabdaa",
+    "url": "/static/media/bouledecampagne-lvc.f28a1c29.JPG"
+  },
+  {
+    "revision": "857cc5dc66259d4c90145d8b895ba480",
+    "url": "/static/media/brightcafet.857cc5dc.JPG"
+  },
+  {
+    "revision": "8e0a5edd2415d8bcd8a137eebfcc07fc",
+    "url": "/static/media/brightcafet.8e0a5edd.JPG"
+  },
+  {
+    "revision": "0ab8334560c3e5b5f5180fcb50b736b3",
+    "url": "/static/media/brightmae.0ab83345.JPG"
+  },
+  {
+    "revision": "acce4d8e3998f825b97230b093d24b14",
+    "url": "/static/media/brightmae.acce4d8e.JPG"
+  },
+  {
+    "revision": "ab9b1e9ab19780ae96f6d2b1fabb5896",
+    "url": "/static/media/brioche feuilletée hervé.ab9b1e9a.JPG"
+  },
+  {
+    "revision": "45a5958a7c8717a9e172fee1e3b3ae0a",
+    "url": "/static/media/briochenature-lvc.45a5958a.JPG"
+  },
+  {
+    "revision": "5f82392162e4b2de4f93cd518f449004",
+    "url": "/static/media/bullesrougesdemadone.5f823921.png"
+  },
+  {
+    "revision": "313c0b089ce2cf4fc82edf50904712b1",
+    "url": "/static/media/catherinepierre.313c0b08.png"
+  },
+  {
+    "revision": "72bac375569bca03b9fefcf9abb8a665",
+    "url": "/static/media/catherinepierrechinon.72bac375.png"
+  },
+  {
+    "revision": "609214333c310e5faa6568dbbf04d06b",
+    "url": "/static/media/chapoutier.60921433.png"
+  },
+  {
+    "revision": "9af2e577f82bad4b06c0412ffe7cc29f",
+    "url": "/static/media/chapoutierCDR.9af2e577.png"
+  },
+  {
+    "revision": "899e7eaa7ae34c95b17761f6ca0cf224",
+    "url": "/static/media/chateaumoulinperonier.899e7eaa.png"
+  },
+  {
+    "revision": "8fc18a3f979f380547dfaa820760278f",
+    "url": "/static/media/clairetteorigine.8fc18a3f.png"
+  },
+  {
+    "revision": "d0344300af1e3c66a1e9463dd3940e26",
+    "url": "/static/media/combucha1.d0344300.JPG"
+  },
+  {
+    "revision": "f64550792f6ca912e8d9612d7e62c0d9",
+    "url": "/static/media/combucha2.f6455079.JPG"
+  },
+  {
+    "revision": "41ac70b81ab70bc393296560a66bd513",
+    "url": "/static/media/damedecleve.41ac70b8.png"
+  },
+  {
+    "revision": "32bbe5539759786491cd691a6a80c7e0",
+    "url": "/static/media/devanture.32bbe553.JPG"
+  },
+  {
+    "revision": "47eb782379869a2cc41da01e1fcdb78b",
+    "url": "/static/media/domaineberthetbondet.47eb7823.png"
+  },
+  {
+    "revision": "06e0edbe227f0b81703c2e4869f5c47f",
+    "url": "/static/media/domainedetreilles.06e0edbe.png"
+  },
+  {
+    "revision": "c7558224b327c1cfd817db01d5dff92b",
+    "url": "/static/media/domaineflorencealquier.c7558224.png"
+  },
+  {
+    "revision": "c5a0206d6e2156b388b0c97c31ec6356",
+    "url": "/static/media/domainelumiantCDR.c5a0206d.png"
+  },
+  {
+    "revision": "ea7e463cd702f40eb7dbfa122721301a",
+    "url": "/static/media/domainelumiantolivierCDR.ea7e463c.png"
+  },
+  {
+    "revision": "21745be90b04519aae28706dd42d791c",
+    "url": "/static/media/drapierlarevallis.21745be9.png"
+  },
+  {
+    "revision": "6a56a3037fa525db7da6ae229912ce9e",
+    "url": "/static/media/drapierrose.6a56a303.png"
+  },
+  {
+    "revision": "f5933434449b2121e83d7377a4b39802",
+    "url": "/static/media/drapiersanssouffre.f5933434.png"
+  },
+  {
+    "revision": "bff927c79aed7929df77dec775185a26",
+    "url": "/static/media/drappierbrutNat.bff927c7.png"
+  },
+  {
+    "revision": "3f7af602a8956eaf9cd64129dbae17bf",
+    "url": "/static/media/echappeebelle.3f7af602.png"
+  },
+  {
+    "revision": "6e63fe20712a5ad61b84489c0edeebdf",
+    "url": "/static/media/empruntesgrisdetoulCDTl.6e63fe20.png"
+  },
+  {
+    "revision": "30eecbdcf197c737228d4e55d87e3c40",
+    "url": "/static/media/epicerie.30eecbdc.jpg"
+  },
+  {
+    "revision": "1af6af05ccfc435258ab203c2f8c6bf1",
+    "url": "/static/media/erickshreiber.1af6af05.png"
+  },
+  {
+    "revision": "df75f2f8be87664e04f7580bcea9887b",
+    "url": "/static/media/erickshreibertradition.df75f2f8.png"
+  },
+  {
+    "revision": "34fced853fbfc31ff69e41a8b67ad33b",
+    "url": "/static/media/florencealquierpuechmourie.34fced85.png"
+  },
+  {
+    "revision": "2135bb1ab73bac607f31e6cd7c006486",
+    "url": "/static/media/florencealquierrieutord.2135bb1a.png"
+  },
+  {
+    "revision": "2acc7cdb81a00ff266de7f7dd1867f1e",
+    "url": "/static/media/gamaydsurvolcan.2acc7cdb.png"
+  },
+  {
+    "revision": "e8a99e52a75c120d04eaf95e70b43658",
+    "url": "/static/media/goodfruits.e8a99e52.JPG"
+  },
+  {
+    "revision": "8502d3b85ec1585bb9069e78730f347c",
+    "url": "/static/media/grolleau.8502d3b8.png"
+  },
+  {
+    "revision": "06d989169589b837604e1826753c9680",
+    "url": "/static/media/guateau.06d98916.JPG"
+  },
+  {
+    "revision": "93b0bf72c35d48d6795ecfa42f4ee2a8",
+    "url": "/static/media/heart.93b0bf72.svg"
+  },
+  {
+    "revision": "4beb9857fd4c33f1e8fce6405d372d55",
+    "url": "/static/media/la-mourette.4beb9857.png"
+  },
+  {
+    "revision": "bae368198b743a445e0446665e96d05e",
+    "url": "/static/media/ladilletante.bae36819.png"
+  },
+  {
+    "revision": "e73f49275c0b54a20ffac5b1a324719c",
+    "url": "/static/media/lamourette.e73f4927.png"
+  },
+  {
+    "revision": "57bd5e0cc8fc387ebb676f05b930d59e",
+    "url": "/static/media/lapoiriere.57bd5e0c.png"
+  },
+  {
+    "revision": "6468634d7642b247f4a6247b94d05553",
+    "url": "/static/media/left-arrow.6468634d.svg"
+  },
+  {
+    "revision": "298ec44a5f7fd667e2f604948d36c526",
+    "url": "/static/media/lelievreCDTl.298ec44a.png"
+  },
+  {
+    "revision": "c5385a3bb836bb60c306a822d719a4cd",
+    "url": "/static/media/lepuy.c5385a3b.png"
+  },
+  {
+    "revision": "756e1399b1ae35308451b8d8bf7a6092",
+    "url": "/static/media/lesmeysonnierCH.756e1399.png"
+  },
+  {
+    "revision": "82f0a07e03a0e3fc5b323b5fb863acfc",
+    "url": "/static/media/lesmeysonnierCH2.82f0a07e.png"
+  },
+  {
+    "revision": "f09bc921f8c3b2e743349a335184808e",
+    "url": "/static/media/livraisons.f09bc921.jpg"
+  },
+  {
+    "revision": "291ee8ee77b7fc1929ef62b2c0ce9b5e",
+    "url": "/static/media/lvc-beauvais.291ee8ee.svg"
+  },
+  {
+    "revision": "20cc28e7d21ea220da38ac899cbe1e33",
+    "url": "/static/media/lvcmap.20cc28e7.png"
+  },
+  {
+    "revision": "be2ee7d2ae630d12b7b1a4582b3a8eb2",
+    "url": "/static/media/macrotomato.be2ee7d2.JPG"
+  },
+  {
+    "revision": "e95d3e60c581e4f0c083897cb3907061",
+    "url": "/static/media/magasin.e95d3e60.JPG"
+  },
+  {
+    "revision": "321ed021553140f8829c9dda1c255342",
+    "url": "/static/media/masdecadenetCDPr.321ed021.png"
+  },
+  {
+    "revision": "f0bb8639e371f9d77160bf032e059679",
+    "url": "/static/media/merlotrouge.f0bb8639.png"
+  },
+  {
+    "revision": "f2b098fa111dd45b5c5c2a6cccc65aa9",
+    "url": "/static/media/modified_IMG_9729.f2b098fa.JPG"
+  },
+  {
+    "revision": "25661ac18215d64cf9bdcaf5032faadc",
+    "url": "/static/media/montiriusCDR.25661ac1.png"
+  },
+  {
+    "revision": "dd16305d31025238d0e1610792902c2a",
+    "url": "/static/media/naturo.dd16305d.JPG"
+  },
+  {
+    "revision": "9bdc4910828864c991d0162263377763",
+    "url": "/static/media/nicestage.9bdc4910.JPG"
+  },
+  {
+    "revision": "35dac676dea19aa37a198847146a0e37",
+    "url": "/static/media/nicestage3.35dac676.JPG"
+  },
+  {
+    "revision": "1ca6aebd8736d22f14872e9736ef4c81",
+    "url": "/static/media/oscar.1ca6aebd.png"
+  },
+  {
+    "revision": "e910291d8e8e585b1616dcbf3d51ec15",
+    "url": "/static/media/pain aux noix herve.e910291d.JPG"
+  },
+  {
+    "revision": "b7923e6eacd812c2961cb77b5f183aa9",
+    "url": "/static/media/pain briochée sesame lvc.b7923e6e.JPG"
+  },
+  {
+    "revision": "6ac5094181b96ce1036c171352b51504",
+    "url": "/static/media/pain des champs lvc.6ac50941.JPG"
+  },
+  {
+    "revision": "23b6f0bc52e9a042339a7688e3b9b278",
+    "url": "/static/media/pain integral lvc.23b6f0bc.JPG"
+  },
+  {
+    "revision": "70ea81a423ccf6db207cb9866a79e98d",
+    "url": "/static/media/painrond1.70ea81a4.JPG"
+  },
+  {
+    "revision": "9bb86d865d118d7b396a5892035e3311",
+    "url": "/static/media/painsbio.9bb86d86.jpg"
+  },
+  {
+    "revision": "9bb86d865d118d7b396a5892035e3311",
+    "url": "/static/media/painsized.9bb86d86.JPG"
+  },
+  {
+    "revision": "6b83bf7d89701d5bc859aec862944d85",
+    "url": "/static/media/painsportif lvc.6b83bf7d.JPG"
+  },
+  {
+    "revision": "50cb6b3ec543b28141a5b348683c8b25",
+    "url": "/static/media/petitchablis.50cb6b3e.png"
+  },
+  {
+    "revision": "25382a358fbf2a180e61dfadc21cdcd8",
+    "url": "/static/media/plant.25382a35.svg"
+  },
+  {
+    "revision": "c8e4f790aab54abbf2e7ead481fe5b70",
+    "url": "/static/media/pontdecayllus.c8e4f790.png"
+  },
+  {
+    "revision": "2d7d4043a254f53458892d50a9ff9ce8",
+    "url": "/static/media/promo.2d7d4043.JPG"
+  },
+  {
+    "revision": "e491982ba44587cddbc6bbed1667cc04",
+    "url": "/static/media/prossecobrut.e491982b.png"
+  },
+  {
+    "revision": "0045bb3a1e3d873e48d83e14a7df69ec",
+    "url": "/static/media/quiche.0045bb3a.JPG"
+  },
+  {
+    "revision": "279196e37844ff702af163035a67c609",
+    "url": "/static/media/right-arrow.279196e3.svg"
+  },
+  {
+    "revision": "d399cbc3270bafaaccfe15b51aa2b83f",
+    "url": "/static/media/sansglutenchataigne.d399cbc3.JPG"
+  },
+  {
+    "revision": "5daf5c45afc6719cdfbd8967b455ec8b",
+    "url": "/static/media/sansglutenmais.5daf5c45.JPG"
+  },
+  {
+    "revision": "b403523f59d1db976ec0c1d4295ee4ad",
+    "url": "/static/media/sansglutenquinoa.b403523f.JPG"
+  },
+  {
+    "revision": "4b1d781d0d42d998b6424a2f990f5988",
+    "url": "/static/media/sansglutensarasin.4b1d781d.JPG"
+  },
+  {
+    "revision": "74457eb404c80443ac9e3e8f31b335e6",
+    "url": "/static/media/soinducorps.74457eb4.jpg"
+  },
+  {
+    "revision": "079cddc79fb3fc2a6601aa8383fdfdb0",
+    "url": "/static/media/soleiller.079cddc7.png"
+  },
+  {
+    "revision": "57bc0706c0bf2864bd4bc7aa625916a6",
+    "url": "/static/media/soleiller25cl.57bc0706.png"
+  },
+  {
+    "revision": "fdec636c374c05a7e46ede85dce8f1dc",
+    "url": "/static/media/souslemont.fdec636c.png"
+  },
+  {
+    "revision": "a26d65f262208914e3067638e9187386",
+    "url": "/static/media/stage1.a26d65f2.JPG"
+  },
+  {
+    "revision": "049585eb832a50d98e48a0637c0cb78a",
+    "url": "/static/media/syrahrosé.049585eb.png"
+  },
+  {
+    "revision": "fac754d03a41239fb6723fac44a8a8fd",
+    "url": "/static/media/trinch.fac754d0.png"
+  },
+  {
+    "revision": "94f1091497443d0a1a1c822a72cd4d79",
+    "url": "/static/media/ubybio.94f10914.png"
+  },
+  {
+    "revision": "8ccb90b7b705e68dd54c2523d98bde6a",
+    "url": "/static/media/vinbio.8ccb90b7.jpg"
+  },
+  {
+    "revision": "e759dba0f67c026a487246dd64882a84",
+    "url": "/static/media/vincentbliard.e759dba0.png"
+  },
+  {
+    "revision": "9b9b187feb6e44ffc85313b353304f65",
+    "url": "/static/media/vinsbackground.9b9b187f.jpg"
+  },
+  {
+    "revision": "cd98e33fdea68f548a1e8bfb420598a4",
+    "url": "/static/media/vrac-bg.cd98e33f.JPG"
+  },
+  {
+    "revision": "58829cc0d7c4349f0908a005a1a88aec",
+    "url": "/static/media/vrac1.58829cc0.JPG"
+  },
+  {
+    "revision": "8e3e41e3ccc95e0dc9c754087d6cf302",
+    "url": "/static/media/vrac2.8e3e41e3.JPG"
+  },
+  {
+    "revision": "f2075d5ea2731a36bc5f8d6df9446fb7",
+    "url": "/static/media/vrac3.f2075d5e.JPG"
+  },
+  {
+    "revision": "cd98e33fdea68f548a1e8bfb420598a4",
+    "url": "/static/media/vracbio.cd98e33f.jpg"
+  },
+  {
+    "revision": "3770673d96b3be7c921bb31b1cc32ce4",
+    "url": "/static/media/watermelon.3770673d.JPG"
+  }
+]);
